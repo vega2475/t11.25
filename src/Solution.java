d00c46c87c744bf94104e325e2d900005965176a
DavidMSK <devid367552@gmail.com>
2023-01-20 13:12:26 +0300

    v.0.9

--- src/Solution.java
+++ src/Solution.java
@@ -3,6 +3,7 @@ public class Solution {
     public static StringBuilder solve(String text, int weight) {
         String[] arrOfParagraph = text.split("\n");
         //String[] arrOfWords = text.split("[. ]");
+        System.out.println(text);
         System.out.println(("--------------------------------------------------------------------------"));//это для того чтобы смотреть в консоли ширину текста и ориентироваться по этой штуку (тут 74 "-")
         StringBuilder sb = new StringBuilder();
 
@@ -13,7 +14,7 @@ public class Solution {
             int quantityOfSymbolsOfWordsInRow = 6;
             int quantityOfSymbolsInEachParagraph = arrOfParagraph[i].length();//for debug
             int quantityOfSpacesInOneRow = 0;
-            if (arrOfParagraph[i].length() > weight) {
+            if(arrOfParagraph[i].length() + 6 > weight) {
                 sb.append("      ");
                 for (int j = 0; quantityOfSymbols < weight && j < arrOfWords1.length; j++) {//проблема здесь от квантити до вейт (должно быть меньше из-за последнего слова проблемы)
                     //if(quantityOfSymbols + arrOfWords1[arrOfWords1.length - 1].length() > weight)break;
@@ -22,25 +23,32 @@ public class Solution {
                     quantityOfWords++;
                     quantityOfSpacesInOneRow = weight - quantityOfSymbolsOfWordsInRow;
 
-                    if (j + 1 < arrOfWords1.length) {
+                    if(j + 1 < arrOfWords1.length) {
                         if (quantityOfSymbols + arrOfWords1[j + 1].length() > weight) {
                             int m = j - quantityOfWords;//индекс слова из массива слов параграфа
-                            int counter = 0;
-                            for (int k = 0; k < quantityOfWords; k++) {
-                                counter++;
-                                m++;
-                                sb.append(arrOfWords1[m]);
-                                int indents = quantityOfWords - 1;//количество промежутков между словами
-                                int spaces = weight - quantityOfSymbolsOfWordsInRow;
-                                int spacesBetweenWords = spaces / indents;
-                                int lastIndentsWithMoreSpaces = spaces % indents;
+                            if (quantityOfWords == 1 && m > -1) {
+                                sb.append(arrOfWords1[m + 1]);
+                            } else if(quantityOfWords == 1 && m <= -1){
+                                sb.append(arrOfWords1[0]);
+                            }
+                            else {
+                                int counter = 0;
+                                for (int k = 0; k < quantityOfWords; k++) {
+                                    counter++;
+                                    m++;
+                                    sb.append(arrOfWords1[m]);
+                                    int indents = quantityOfWords - 1;//количество промежутков между словами
+                                    int spaces = weight - quantityOfSymbolsOfWordsInRow;
+                                    int spacesBetweenWords = spaces / indents;
+                                    int lastIndentsWithMoreSpaces = spaces % indents;
 
 
-                                for (int q = 0; q < spacesBetweenWords && counter <= quantityOfWords - (lastIndentsWithMoreSpaces + 1); q++) {
-                                    sb.append(" ");
-                                }
-                                for (int e = 0; e < spacesBetweenWords + 1 && counter > quantityOfWords - (lastIndentsWithMoreSpaces + 1); e++) {
-                                    sb.append(" ");
+                                    for (int q = 0; q < spacesBetweenWords && counter <= quantityOfWords - (lastIndentsWithMoreSpaces + 1); q++) {
+                                        sb.append(" ");
+                                    }
+                                    for (int e = 0; e < spacesBetweenWords + 1 && counter > quantityOfWords - (lastIndentsWithMoreSpaces + 1); e++) {
+                                        sb.append(" ");
+                                    }
                                 }
                             }
                             quantityOfSpacesInOneRow = 0;
@@ -49,12 +57,11 @@ public class Solution {
                             quantityOfWords = 0;
                             sb.append("\n");
                         }
-                    } else if (j == arrOfWords1.length - 1) {//для допечатки длинных абзацев строки которых переносятся из-за их размеры
-                        for (int s = quantityOfWords - 1; s >= 0; s--) {
+
+                    }
+                    else if(j == arrOfWords1.length - 1){//для допечатки длинных абзацев строки которых переносятся из-за их размеры
+                        for(int s = quantityOfWords - 1; s >= 0; s--){
                             sb.append(arrOfWords1[j - s]).append(" ");
-                            int indents = quantityOfWords - 1;//количество промежутков между словами
-                            int spacesBetweenWords = quantityOfSpacesInOneRow / indents;//количество пробелов между словами
-                            int lastWordsWithMoreSpaces = quantityOfSpacesInOneRow % indents;//количество последних слов между которыми кол-во пробелов должо быть на 1 больше чем между остальными в этой строке
 
                         }
                     }
@@ -63,7 +70,8 @@ public class Solution {
                 }
 
                 sb.append("\n");
-            } else {
+            }
+            else{
                 sb.append("      ").append(arrOfParagraph[i]).append("\n");
             }
         }
