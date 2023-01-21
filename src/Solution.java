7dcf14144def8b837fc56af585c356c5b060e629
DavidMSK <devid367552@gmail.com>
2023-01-21 19:28:55 +0300

    v.01

--- src/Solution.java
+++ src/Solution.java
@@ -2,7 +2,6 @@ public class Solution {
 
     public static StringBuilder solve(String text, int weight) {
         String[] arrOfParagraph = text.split("\n");
-        //String[] arrOfWords = text.split("[. ]");
         System.out.println(text);
         System.out.println(("--------------------------------------------------------------------------"));//это для того чтобы смотреть в консоли ширину текста и ориентироваться по этой штуку (тут 74 "-")
         StringBuilder sb = new StringBuilder();
@@ -12,26 +11,21 @@ public class Solution {
             int quantityOfSymbols = 6;
             int quantityOfWords = 0;
             int quantityOfSymbolsOfWordsInRow = 6;
-            int quantityOfSymbolsInEachParagraph = arrOfParagraph[i].length();//for debug
-            int quantityOfSpacesInOneRow = 0;
-            if(arrOfParagraph[i].length() + 6 > weight) {
+            if (arrOfParagraph[i].length() + 6 > weight) {
                 sb.append("      ");
                 for (int j = 0; quantityOfSymbols < weight && j < arrOfWords1.length; j++) {//проблема здесь от квантити до вейт (должно быть меньше из-за последнего слова проблемы)
-                    //if(quantityOfSymbols + arrOfWords1[arrOfWords1.length - 1].length() > weight)break;
                     quantityOfSymbols += arrOfWords1[j].length() + 1; //подсчет кол-ва символов для каждой строки включая красную строку (ее пишем как 6 пробелов)
                     quantityOfSymbolsOfWordsInRow += arrOfWords1[j].length();
                     quantityOfWords++;
-                    quantityOfSpacesInOneRow = weight - quantityOfSymbolsOfWordsInRow;
 
-                    if(j + 1 < arrOfWords1.length) {
+                    if (j + 1 < arrOfWords1.length) {
                         if (quantityOfSymbols + arrOfWords1[j + 1].length() > weight) {
                             int m = j - quantityOfWords;//индекс слова из массива слов параграфа
                             if (quantityOfWords == 1 && m > -1) {
                                 sb.append(arrOfWords1[m + 1]);
-                            } else if(quantityOfWords == 1 && m <= -1){
+                            } else if (quantityOfWords == 1 && m <= -1) {
                                 sb.append(arrOfWords1[0]);
-                            }
-                            else {
+                            } else {
                                 int counter = 0;
                                 for (int k = 0; k < quantityOfWords; k++) {
                                     counter++;
@@ -51,16 +45,14 @@ public class Solution {
                                     }
                                 }
                             }
-                            quantityOfSpacesInOneRow = 0;
                             quantityOfSymbolsOfWordsInRow = 0;
                             quantityOfSymbols = 0;
                             quantityOfWords = 0;
                             sb.append("\n");
                         }
 
-                    }
-                    else if(j == arrOfWords1.length - 1){//для допечатки длинных абзацев строки которых переносятся из-за их размеры
-                        for(int s = quantityOfWords - 1; s >= 0; s--){
+                    } else if (j == arrOfWords1.length - 1) {//для допечатки длинных абзацев строки которых переносятся из-за их размеры
+                        for (int s = quantityOfWords - 1; s >= 0; s--) {
                             sb.append(arrOfWords1[j - s]).append(" ");
 
                         }
@@ -70,8 +62,7 @@ public class Solution {
                 }
 
                 sb.append("\n");
-            }
-            else{
+            } else {
                 sb.append("      ").append(arrOfParagraph[i]).append("\n");
             }
         }
